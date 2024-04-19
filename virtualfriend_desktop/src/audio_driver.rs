4f4835153c59778a6103c09b9c8b11d83caa4786
Adam Gastineau <adam@agg.im>
2024-04-19 12:14:39 -0700

    Attempt to resample using rubato

--- virtualfriend_desktop/src/audio_driver.rs
+++ virtualfriend_desktop/src/audio_driver.rs
@@ -1,8 +1,12 @@
-use std::{cmp::Ordering, collections::VecDeque};
+use std::{cmp::Ordering, collections::VecDeque, time::Instant};
 
 use cpal::{
     traits::{DeviceTrait, HostTrait, StreamTrait},
     OutputCallbackInfo, Stream,
+};
+use rubato::{
+    FftFixedInOut, FftFixedOut, Resampler, SincFixedOut, SincInterpolationParameters,
+    SincInterpolationType, WindowFunction,
 };
 use virtualfriend::vsu::traits::AudioFrame;
 
@@ -45,59 +49,114 @@ impl AudioDriver {
             })
             .expect("Could not find a suitable sample rate");
 
+        println!("Resampling to {}", config.max_sample_rate().0);
+
         let buffer_frames = (sample_rate * desired_latency_ms / 1000 * 2) as usize;
 
-        // let buffer = Arc::new(Mutex::new(VecDeque::<AudioFrame>::new()));
-
-        // let config = StreamConfig {
-        //     channels: config.channels(),
-        //     sample_rate: config.min_sample_rate(),
-        //     buffer_size: BufferSize::Fixed(buffer_frames as u32),
-        // };
-
-        let mut buffer = VecDeque::<VecDeque<AudioFrame>>::new();
+        // let mut resampler: Option<SincFixedOut<f32>> = None;
+        let mut resampler: Option<FftFixedInOut<f32>> = None;
+        let mut resample_buffers: Option<(Vec<Vec<f32>>, Vec<Vec<f32>>)> = None;
 
         let mut initial_buffer = true;
 
         let stream = output_device
             .build_output_stream(
-                // &config.with_max_sample_rate().into(),
-                &output_device.default_output_config().unwrap().into(),
+                &config.with_max_sample_rate().into(),
                 move |data: &mut [f32], _: &OutputCallbackInfo| {
-                    let frame_buffer = on_frame_request(data.len());
+                    // let local_resampler = resampler.get_or_insert_with(|| {
+                    //     SincFixedOut::new(
+                    //         (config.max_sample_rate().0 as f64) / (sample_rate as f64),
+                    //         10.0,
+                    //         SincInterpolationParameters {
+                    //             sinc_len: 256,
+                    //             f_cutoff: 0.95,
+                    //             oversampling_factor: 128,
+                    //             interpolation: SincInterpolationType::Quadratic,
+                    //             window: WindowFunction::Blackman,
+                    //         },
+                    //         data.len() / 2,
+                    //         2,
+                    //     )
+                    //     .unwrap()
+                    // });
+                    // let local_resampler = resampler.get_or_insert_with(|| {
+                    //     FftFixedOut::new(
+                    //         sample_rate as usize,
+                    //         config.max_sample_rate().0 as usize,
+                    //         data.len() / 2 as usize,
+                    //         100,
+                    //         2,
+                    //     )
+                    //     .unwrap()
+                    // });
+                    let local_resampler = resampler.get_or_insert_with(|| {
+                        FftFixedInOut::new(
+                            sample_rate as usize,
+                            config.max_sample_rate().0 as usize,
+                            484,
+                            2,
+                        )
+                        .unwrap()
+                    });
 
-                    if initial_buffer {
-                        initial_buffer = false;
+                    let (input_resample_buffer, output_resample_buffer) = resample_buffers
+                        .get_or_insert_with(|| {
+                            (
+                                local_resampler.input_buffer_allocate(false),
+                                vec![vec![0.0f32; local_resampler.output_frames_max()]; 2],
+                            )
+                        });
 
-                        println!("Writing initial buffer");
-
-                        for value in data.iter_mut() {
-                            *value = 0.0;
-                        }
-
-                        return;
-                    }
+                    println!("Frames requested: {}", local_resampler.input_frames_next());
+                    let frame_buffer = on_frame_request(local_resampler.input_frames_next());
 
                     // Save buffer
-                    buffer.push_back(frame_buffer);
+                    input_resample_buffer[0].clear();
+                    input_resample_buffer[1].clear();
 
-                    if let Some(mut frame_buffer) = buffer.pop_front() {
-                        // Copy frames from emulator buffer to soundcard buffer
-                        for frame in data.chunks_mut(2) {
-                            let mut frame_iter = frame.iter_mut();
-                            let channel0 = frame_iter.next().unwrap();
-                            let channel1 = frame_iter.next().unwrap();
+                    for frame in frame_buffer.into_iter() {
+                        input_resample_buffer[0].push((frame.0 as f32) / 32768.0);
+                        input_resample_buffer[1].push((frame.1 as f32) / 32768.0);
+                    }
 
-                            // Remove entries from buffer as we use them
-                            if let Some(incoming_frame) = frame_buffer.pop_front() {
-                                *channel0 = (incoming_frame.0 as f32) / 32768.0;
-                                *channel1 = (incoming_frame.1 as f32) / 32768.0;
-                            } else {
-                                println!("Attempted to copy buffer that does not exist");
-                            }
-                        }
-                    } else {
-                        println!("No audio frame to send. Buffer underrun");
+                    // Resample to system format
+                    local_resampler
+                        .process_into_buffer(
+                            input_resample_buffer.as_slice(),
+                            output_resample_buffer.as_mut_slice(),
+                            None,
+                        )
+                        .expect("Could not resample");
+
+                    // if initial_buffer {
+                    //     initial_buffer = false;
+
+                    //     println!("Writing initial buffer");
+
+                    //     for value in data.iter_mut() {
+                    //         *value = 0.0;
+                    //     }
+
+                    //     return;
+                    // }
+
+                    // Copy frames from emulator buffer to soundcard buffer
+                    for (output_frame, input_frame) in
+                        data.chunks_mut(2).zip(output_resample_buffer.iter())
+                    {
+                        let mut frame_iter = output_frame.iter_mut();
+                        let channel0 = frame_iter.next().unwrap();
+                        let channel1 = frame_iter.next().unwrap();
+
+                        // Remove entries from buffer as we use them
+                        *channel0 = input_frame[0];
+                        *channel1 = input_frame[1];
+
+                        // println!("{channel0}");
+
+                        // if *channel0 > 0.0 {
+                        //     println!("{channel0}");
+                        // }
                     }
                 },
                 move |err| println!("Audio error: {err}"),
---
+++ virtualfriend_desktop/src/linear_resampler.rs
@@ -0,0 +1,104 @@
+// From the Rustual Boy project
+// Copyright (c) 2016-2020 Jake Taylor
+//
+// Permission is hereby granted, free of charge, to any
+// person obtaining a copy of this software and associated
+// documentation files (the "Software"), to deal in the
+// Software without restriction, including without
+// limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of
+// the Software, and to permit persons to whom the Software
+// is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice
+// shall be included in all copies or substantial portions
+// of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF
+// ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+// TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+// PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+// SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+// IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+struct LinearResampler {
+    from_sample_rate: u32,
+    to_sample_rate: u32,
+
+    current_from_frame: AudioFrame,
+    next_from_frame: AudioFrame,
+    from_fract_pos: u32,
+
+    current_frame_channel_offset: u32,
+}
+
+impl LinearResampler {
+    fn new(from_sample_rate: u32, to_sample_rate: u32) -> LinearResampler {
+        let sample_rate_gcd = {
+            fn gcd(a: u32, b: u32) -> u32 {
+                if b == 0 {
+                    a
+                } else {
+                    gcd(b, a % b)
+                }
+            }
+
+            gcd(from_sample_rate, to_sample_rate)
+        };
+
+        LinearResampler {
+            from_sample_rate: from_sample_rate / sample_rate_gcd,
+            to_sample_rate: to_sample_rate / sample_rate_gcd,
+
+            current_from_frame: (0, 0),
+            next_from_frame: (0, 0),
+            from_fract_pos: 0,
+
+            current_frame_channel_offset: 0,
+        }
+    }
+
+    fn next(&mut self, input: &mut dyn Iterator<Item = i16>) -> i16 {
+        fn interpolate(a: i16, b: i16, num: u32, denom: u32) -> i16 {
+            (((a as i32) * ((denom - num) as i32) + (b as i32) * (num as i32)) / (denom as i32))
+                as _
+        }
+
+        let ret = match self.current_frame_channel_offset {
+            0 => interpolate(
+                self.current_from_frame.0,
+                self.next_from_frame.0,
+                self.from_fract_pos,
+                self.to_sample_rate,
+            ),
+            _ => interpolate(
+                self.current_from_frame.1,
+                self.next_from_frame.1,
+                self.from_fract_pos,
+                self.to_sample_rate,
+            ),
+        };
+
+        self.current_frame_channel_offset += 1;
+        if self.current_frame_channel_offset >= 2 {
+            self.current_frame_channel_offset = 0;
+
+            self.from_fract_pos += self.from_sample_rate;
+            while self.from_fract_pos > self.to_sample_rate {
+                self.from_fract_pos -= self.to_sample_rate;
+
+                self.current_from_frame = self.next_from_frame;
+
+                let left = input.next().unwrap_or(0);
+                let right = input.next().unwrap_or(0);
+                self.next_from_frame = (left, right);
+            }
+        }
+
+        ret
+    }
+}
