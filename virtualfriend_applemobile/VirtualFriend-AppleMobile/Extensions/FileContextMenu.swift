b53234df9657a46ebea0314081590ce955566adf
Adam Gastineau <adam@agg.im>
2026-08-08 09:58:40 -0700

    Fix build

---
+++ virtualfriend_applemobile/VirtualFriend-AppleMobile/Extensions/FileContextMenu.swift
@@ -0,0 +1,18 @@
+//
+//  FileContextMenu.swift
+//  VirtualFriend
+//
+//  Created by Adam Gastineau on 8/8/26.
+//
+
+import SwiftUI
+
+struct FileContextMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FileContextMenu()
+}
